9bac7f31956ff746fbf7799c5cc39c7d607c8c55
hari6135 <hariabi6135@gmail.com>
2026-04-28 09:25:29 +0530

    day 5 commit

---
+++ PythonProject/Basic_programs/regexeg.py
@@ -0,0 +1,13 @@
+import re
+
+# digits
+#
+
+# lowercase
+# string = input('Enter the pattern: ')
+# pat = r"^[a-z]$"
+# if re.match(pattern=pat, string=string):
+#     print('correct pattern found!!')
+#
+# else:
+#     print('NOT found!!')
