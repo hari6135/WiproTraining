003e9291942eec350f19473b1a6d3140365a3275
hari6135 <hariabi6135@gmail.com>
2026-05-04 18:45:25 +0530

    selenium day 9 commit

--- Selenium/Automation Basics/selenium_basics/google_homepage.py
+++ Selenium/Automation Basics/selenium_basics/google_homepage.py
@@ -1,11 +1,12 @@
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 browser = input("Enter the desired browser: ")
 match(browser.lower()):
     case 'chrome':
-        driver = webdriver.Chrome(service=Service('../resources/chromedriver.exe'))
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
     case 'edge':
         driver = webdriver.Edge(service=Service('../resources/msedgedriver.exe'))
